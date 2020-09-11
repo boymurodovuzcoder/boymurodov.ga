c0d8b7644ee48f5b49137e164b28bc8d4c58737c
Samandar <boymurodovuzcoder@gmail.com>
2020-09-12 01:28:17 +0500

    added google maps

--- js/main.js
+++ js/main.js
@@ -297,38 +297,189 @@
     };
 
 
-    // /* Contact Form
+    // /* Contact Form $("#form-id").submit();
     //  * ------------------------------------------------------ */
-    // var ssContactForm = function() {
+    function Validation() {
 
-    //     /* local validation */
-    //     $('#contactForm').validate({
+        /* local validation */
+        $('#contactForm').validate(
+            //	{
+            //     submitHandler: function(form) {
+            //         var sLoader = $('#submit-loader');
+            //         sLoader.fadeIn();
 
-    //         /* submit via ajax */
-    //         submitHandler: function(form) {
-    //             var sLoader = $('#submit-loader');
-    //             sLoader.fadeIn();
+            //         function delay(delayInms) {
+            //             return new Promise(resolve => {
+            //                 setTimeout(() => {
+            //                     resolve(2);
+            //                 }, delayInms);
+            //             });
+            //         }
 
-    //             function delay(delayInms) {
-    //                 return new Promise(resolve => {
-    //                     setTimeout(() => {
-    //                         resolve(2);
-    //                     }, delayInms);
-    //                 });
-    //             }
+            //         async function sample() {
+            //             let delayres = await delay(1500);
+            //             sLoader.fadeOut();
+            //             $('#message-warning').hide();
+            //             $('#contactForm').fadeOut();
+            //             $('#contactForm').submit();
+            //             $('#message-success').fadeIn();
+            //         }
+            //         sample();
+            //     }
+            // }
+        );
+    };
+    $(".submitform").click(function() {
+        var sLoader = $('#submit-loader');
 
-    //             async function sample() {
-    //                 let delayres = await delay(1500);
-    //                 sLoader.fadeOut();
-    //                 $('#message-warning').hide();
-    //                 $('#contactForm').fadeOut();
-    //                 $('#message-success').fadeIn();
-    //             }
-    //             sample();
-    //         }
-    //     });
-    // };
+        function delay(delayInms) {
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    resolve(2);
+                }, delayInms);
+            });
+        }
+        async function sample() {
+            let delayres = await delay(1500);
+            sLoader.fadeOut();
+        }
+        sample();
+        sLoader.fadeIn();
+        Validation()
+    })
 
+    /* Google Maps
+     * ------------------------------------------------------ */
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: 41.377491, lng: 69.585258 },
+            zoom: 6,
+            styles: [
+                { elementType: 'geometry', stylers: [{ color: '#ebe3cd' }] },
+                { elementType: 'labels.text.fill', stylers: [{ color: '#523735' }] },
+                { elementType: 'labels.text.stroke', stylers: [{ color: '#f5f1e6' }] },
+                {
+                    featureType: 'administrative',
+                    elementType: 'geometry.stroke',
+                    stylers: [{ color: '#c9b2a6' }]
+                },
+                {
+                    featureType: 'administrative.land_parcel',
+                    elementType: 'geometry.stroke',
+                    stylers: [{ color: '#dcd2be' }]
+                },
+                {
+                    featureType: 'administrative.land_parcel',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#ae9e90' }]
+                },
+                {
+                    featureType: 'landscape.natural',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#dfd2ae' }]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#dfd2ae' }]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#93817c' }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry.fill',
+                    stylers: [{ color: '#a5b076' }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#447530' }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#f5f1e6' }]
+                },
+                {
+                    featureType: 'road.arterial',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#fdfcf8' }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#f8c967' }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{ color: '#e9bc62' }]
+                },
+                {
+                    featureType: 'road.highway.controlled_access',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#e98d58' }]
+                },
+                {
+                    featureType: 'road.highway.controlled_access',
+                    elementType: 'geometry.stroke',
+                    stylers: [{ color: '#db8555' }]
+                },
+                {
+                    featureType: 'road.local',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#806b63' }]
+                },
+                {
+                    featureType: 'transit.line',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#dfd2ae' }]
+                },
+                {
+                    featureType: 'transit.line',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#8f7d77' }]
+                },
+                {
+                    featureType: 'transit.line',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{ color: '#ebe3cd' }]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#dfd2ae' }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry.fill',
+                    stylers: [{ color: '#b9d3c2' }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#92998d' }]
+                }
+            ]
+        })
+        var myLatLng = { lat: 40.9937545, lng: 69.3636634 };
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map
+        });
+        var infowindow = new google.maps.InfoWindow({
+            content: "<div style='color: #000000;'>My Location<div>"
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            map.panTo(this.getPosition());
+            map.setZoom(9);
+            infowindow.open(map, marker)
+        });
+    }
+    initMap()
 
     /* Back to Top
      * ------------------------------------------------------ */
@@ -369,8 +520,6 @@
         ssAlertBoxes();
         ssAnimations();
         ssIntroAnimation();
-        ssContactForm();
-        ssAjaxChimp();
         ssBackToTop();
 
     })();
